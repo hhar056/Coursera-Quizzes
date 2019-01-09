65ff989e689f0996cd476cd96fdf66bc5dca42cf
Humphrey Hart <humphreyhart@gmail.com>
2019-01-09 15:45:15 +1300

    week 1 done

--- C3Q1.R
+++ C3Q1.R
@@ -1,4 +1,5 @@
 getwd()
+setwd("Coursera Quizzes")
 download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06hid.csv","2006 Housing Survey")
 houseData<-read.csv("2006 Housing Survey")
 head(houseData)
@@ -10,12 +11,34 @@ result
 install.packages("rJava")
 install.packages("xlsxjars")
 install.packages("xlsx")
+install.packages("XML")
 library("rJava")
 library("xlsxjars")
 library("xlsx")
+library("XML")
 
 
-download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FDATA.gov_NGAP.xlsx","C3Q1/Natural Gas.xlsx")
-dat<-read.xlsx("C3Q1/Natural Gas.xlsx",sheetIndex=1,rowIndex=18:23,colIndex=7:15)
+download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FDATA.gov_NGAP.xlsx","Natural Gas.xlsx", mode="wb")
+dat<-read.xlsx("Natural Gas.xlsx",sheetIndex=1,rowIndex=18:23,colIndex=7:15)
 head(dat)
 sum(dat$Zip*dat$Ext,na.rm=T)
+
+download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Frestaurants.xml","Restaurants.xml", mode="wb")
+dat<-xmlTreeParse("Restaurants.xml", useInternal=TRUE)
+head(dat)
+
+
+topNode <- xmlRoot(dat)
+xmlName(topNode)
+topNode[1]
+topNode[[1]][[1]]
+
+els<-getNodeSet(topNode,"//row/row")
+els
+test<-xmlSApply(els,extraFun)
+class(test)
+head(test)
+
+extraFun<-function(x){
+    xmlSApply(x,xmlValue)
+}
